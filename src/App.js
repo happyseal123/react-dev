4ec429330e6d5ccf26748f64c7c7ea5523e61411
mmenz01 <113487922+happyseal123@users.noreply.github.com>
2022-11-29 23:27:39 -0500

    buggy with most functionality

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import RecipeItem from "./components/RecipeItem";
 
 function App() {
 
-  // const [items, setItem] = useState([]);
-  // const [total, setTotal] = useState(0);
-
   const [saved, setSaved] = useState([]);
   const [cookTime, setCookTime] = useState(0);
 
@@ -24,22 +21,93 @@ function App() {
     setCookTime(cookTime-time);
   }
 
+//   const [recipes, setRecipes] = useState([]);
+//   const setUpRecipes = () => {
+//     setRecipes(["hey"]);
+//  }
+
+  const [recipeDataState, setRecipeDataState] = useState(recipeData);
+
+  const [opac, setOpac] = useState(1);
+  const [mealColors, setMealColors] = useState(["black", "black", "black"]);
+
   return (
     <div className="App">
       <h1>Website Name</h1>
+
+      <button onClick={() => {
+
+        // BELOW IS BUGGY, IDK WHY
+        
+        // setRecipeDataState(
+        //   recipeDataState.sort((a,b) => {
+        //     if (a.preptime > b.preptime) {
+        //       return 1;
+        //     }
+        //     if (a.preptime < b.preptime) {
+        //       return -1;
+        //     }
+        //     return 0;
+        //   }
+        // ));
+            }
+                }>Sort By Ascending Prep Time
+      </button>
+
+      <button id="reset" onClick={() => {
+        setOpac(1);
+        setMealColors(["black", "black", "black"]);
+        setRecipeDataState(recipeData);
+      }}>
+        Reset Sorting & All Filters
+      </button>
+
+      {/* {recipeData[0].name} */}
+
+    <div>
+    <h4>Meal Type:</h4>
+    <button style={{ opacity : opac , color: mealColors[0] }} onClick={() => {
+      setRecipeDataState(recipeDataState.filter(item => item.mealtype == "Breakfast"));
+      setOpac(0.6);
+      setMealColors(["red", "black", "black"]);
+    }}>Breakfast</button>
+    <button style={{opacity:opac}}>Lunch</button>
+    <button style={{opacity:opac}}>Dinner</button>
+    </div>
+
+    <div>
+    <h4>Difficulty Level</h4>
+    <button onClick={() => {
+      setRecipeDataState(recipeDataState.filter(item => item.difficulty == "Beginner"));
+    }}>Beginner</button>
+    <button>Advanced</button>
+    </div>
+    
       
       <div id="body">
-        <div id="recipes">
+
+        {/* <div id="recipes">
           <h2>Recipes:</h2>
           {recipeData.map((item) => (
             <RecipeItem prop1={item} addClick={saveRecipe} removeClick={removeRecipe}/>
           ))}
+        </div> */}
+
+        <div id="recipes">
+          <h2>Recipes:</h2>
+
+          {recipeDataState.map((item) => (
+            <RecipeItem prop1={item} addClick={saveRecipe} removeClick={removeRecipe}/>
+          ))}
+
         </div>
+
         <div>
           <h2>Recipe Box:</h2>
 
 
           <h3>Saved Recipes:</h3>
+
           {saved.map((recipe) => (
             <p>{recipe}</p>
           ))}
--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -22,9 +22,9 @@ export default function RecipeItem({prop1, addClick, removeClick}) {
         <div>
             <img src={prop1.image}/>
             <h2> {prop1.name}</h2>
-            <h3>{prop1.mealtype}</h3>
-            <h3>{prop1.difficulty}</h3>
-            <h3>{prop1.preptime} Minutes</h3>
+            <h3>Meal Type: {prop1.mealtype}</h3>
+            <h3>Difficulty Level: {prop1.difficulty}</h3>
+            <h3>Prep Time: {prop1.preptime} Minutes</h3>
 
             <button style={{display:addVis}} onClick={() => {
                 addClick(prop1.name, prop1.preptime);
